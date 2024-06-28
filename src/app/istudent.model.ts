266fcdd570cdc181a2990981512521e091d740b9
Suraysen Thiyagarasu <suraysen@yahoo.com>
2024-06-28 23:15:46 +0800

    master

---
+++ src/app/istudent.model.ts
@@ -0,0 +1,7 @@
+export interface IStudent {
+    Name: string ;
+    Branch: string ;
+    Mobile: number ;
+    Gender: string;
+    Age: number ;
+}
---
+++ src/app/student/student.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { StudentModel } from "../student-model.model";
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrl: './student.component.css'
+})
+export class StudentComponent {
+  @Input() studentList : StudentModel[]=[];
+
+  students: StudentModel[] = [
+    new StudentModel("Suraysen", "KL", 60122330102,"Male",48),
+    new StudentModel("Nirmala", "NS", 60122759484,"Female",44),
+    new StudentModel("Kavina", "KL", 10000,"Female",17),
+  ];
+  
+
+}
